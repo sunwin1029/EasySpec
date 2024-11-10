51221777aeda35e91094aecc1f44152ffbf8398d
sunwin1029 <hsseo1029@naver.com>
2024-11-10 16:21:16 +0900

    1차 테스트 중

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,9 @@ android {
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
-
+    viewBinding {
+        enable = true
+    }
     buildTypes {
         release {
             isMinifyEnabled = false
