d6845c9c4ce8911e69cb156743dae011dce33220
sunwin1029 <hsseo1029@naver.com>
2024-11-24 21:29:47 +0900

    복구

--- app/src/main/java/com/example/easyspec/TabletItem.java
+++
@@ -1,9 +0,0 @@
-package com.example.easyspec;
-
-public class TabletItem extends ProductItem{
-    private Review screenReview;
-    private Review batteryReview;
-    private Review weightReview;
-    private Review pencilReview;
-    private Review featureReview;
-}
