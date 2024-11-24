07ae3fb46d4da68e5dc0cade1df252ea587b0fdc
sunwin1029 <hsseo1029@naver.com>
2024-11-25 01:20:24 +0900

    타브랜치 접속 위해 커밋

--- app/src/main/java/com/example/easyspec/Firebase/FirebaseHelper.java
+++ app/src/main/java/com/example/easyspec/Firebase/FirebaseHelper.java
@@ -5,7 +5,7 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.database.DatabaseError;
-import com.example.easyspec.Data.ProductItem;
+
 
 import java.util.ArrayList;
 import java.util.List;
