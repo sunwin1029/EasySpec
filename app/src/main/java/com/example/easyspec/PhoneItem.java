c01f008a51621f78fdc7f1a7fa3a828f89ee5f19
sunwin1029 <hsseo1029@naver.com>
2024-11-24 21:46:50 +0900

    복구 완료

--- app/src/main/java/com/example/easyspec/PhoneItem.java
+++
@@ -1,9 +0,0 @@
-package com.example.easyspec;
-
-public class PhoneItem extends ProductItem{
-    private Review batteryReview;
-    private Review performanceReview;
-    private Review cameraReview;
-    private Review weightReview;
-    private Review featureReview;
-}
--- app/src/main/java/com/example/easyspec/Review.java
+++
@@ -1,7 +0,0 @@
-package com.example.easyspec;
-
-public class Review {
-    private String userID;
-    private String reviewString;
-    private int goodNum;  // 좋아요 숫자
-}
